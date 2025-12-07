6e6d4165a584b7ad8652aadba80cac441c83fff8
Charli Harrold <charliharrold@gmail.com>
2025-12-07 14:05:19 -0800

    Add gradient animation to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
+import './Home.css';
+
 const Home = () => {
     return (
-        <div>
-            <h1>I'm Charli</h1>
-            <h2>Welcome to my website</h2>
-            <p>I'm a Computer Science and Mathematics Honours student at the University of Victoria!</p>
+        <div className="home-container">
+            <div className="content">
+                <br/><br/>
+                <h1>I'm Charli</h1>
+                <h2>Welcome to my website</h2>
+                <p>I'm a Computer Science and Mathematics Honours student at the University of Victoria!</p>
+            </div>
         </div>
     )
 }
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,10 +2,11 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {Link} from 'react-router-dom';
+import './NavigationBar.css';
 
 function NavigationBar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" sticky="top" className="custom-navbar">
       <Container>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
