0800218d121690fe7316eac4bdb6ff4bfef2d86b
Charli Harrold <charliharrold@gmail.com>
2025-12-07 12:45:21 -0800

    Github pages setup

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>I'm Charli</h1>
+            <h2>Welcome to my website</h2>
+            <p>I'm a Computer Science and Mathematics Honours student at the University of Victoria!</p>
+        </div>
+    )
+}
+
+export default Home;
