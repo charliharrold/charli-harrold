7ea0e2e95379305356edb09ba65d9cd27f812448
Charli Harrold <charliharrold@gmail.com>
2025-12-09 17:39:08 -0800

    Lots of cute updates

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,14 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './NavigationBar.css';
+import pinkComputer from '../assets/pink-computer-transparent.png';
 
 function NavigationBar() {
   return (
     <Navbar expand="lg" sticky="top" className="custom-navbar">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          <img src={pinkComputer} style={{ width: '75px', height: '75px', objectFit: 'cover', marginRight: '10px' }} alt='Cute pink computer' />
+          Charli Harrold
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
