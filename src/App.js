4b58ebd030b23bb3f367d92210abea4aa5223251
Charli Harrold <charliharrold@gmail.com>
2025-12-09 18:02:26 -0800

    Add some additional pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import NavigationBar from './components/NavigationBar';
 import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
 import './App.css';
 
 const App = () => {
@@ -9,6 +11,8 @@ const App = () => {
       <NavigationBar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/projects' element={<Projects />} />
       </Routes>
     </Router>
   )
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -18,6 +18,9 @@ function NavigationBar() {
           <Nav className="me-auto">
             <Nav.Link as={Link} to='/'>Home</Nav.Link>
             <Nav.Link as={Link} to='/about'>About</Nav.Link>
+            <Nav.Link as={Link} to='/projects'>Projects</Nav.Link>
+            <Nav.Link as={Link} to='/work-experience'>Work Experience</Nav.Link>
+            <Nav.Link as={Link} to='/volunteering'>Volunteering</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
